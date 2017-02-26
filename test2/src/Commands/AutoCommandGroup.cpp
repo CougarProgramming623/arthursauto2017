95eb78942204f7bf68b9f200cdc8331c0fbe34be
arthurliao <liaoarthur@yahoo.com>
2017-02-26 13:24:04 -0500

    .

--- test2/src/Commands/AutoCommandGroup.cpp
+++ test2/src/Commands/AutoCommandGroup.cpp
@@ -15,44 +15,75 @@ AutoCommandGroup::AutoCommandGroup(int option1) : CommandGroup() {
 		return;
 
 	AddSequential(new DistanceDriveCommand2(24.0)); //24 inches
-//		AddSequential(new Turn(-59.9f));
+		AddSequential(new Turn(59.9f));
 		AddSequential(new DistanceDriveCommand2(12.0));
 
-	/*AddSequential(new SolenoidClaspCommand(0, 0));
-	AddSequential(new WaitTime(2));
-	AddSequential(new SolenoidClaspCommand(0, 1));
-	AddSequential(new WaitTime(3));
-	AddSequential(new DistanceDriveCommand2(2.0));
-	AddSequential(new Turn(-59.9f));
-	AddSequential(new DistanceDriveCommand2(1.0));
+	/*
+..
+	    AddSequential(new SolenoidClaspCommand(0, 0));
+		AddSequential(new WaitTime(0.5));
+		AddSequential(new SolenoidClaspCommand(0, 1));
+		AddSequential(new WaitTime(0.5));
+		AddSequential(new DistanceDriveCommand2(24));
+		AddSequential(new Turn(59.9f));
+		AddSequential(new DistanceDriveCommand2(12));
+		AddSequential(new WaitTime(0.5));
+		AddSequential(new SolenoidClaspCommand(1, 0));
 
-	*/
-    //option will be 0, 1, 2
+		*/
 
-//	AddSequential(new DistanceDriveCommand2(1.0));
+		if(1==1)
+			return;
 
+		//REAL SCENORY
+		if(option==0) //in the field left sided, and try to turn right
+		{
+			    AddSequential(new SolenoidClaspCommand(0, 0));
+				AddSequential(new WaitTime(0.5));
+				AddSequential(new SolenoidClaspCommand(0, 1));
+				AddSequential(new WaitTime(0.5));
+				AddSequential(new DistanceDriveCommand2(100.6));
+				AddSequential(new Turn(59.9f));
+				AddSequential(new DistanceDriveCommand2(15.18));
+				AddSequential(new WaitTime(0.5));
+				AddSequential(new SolenoidClaspCommand(1, 0));
+				AddSequential(new WaitTime(0.5));
+				AddSequential(new DistanceDriveCommand2(-15.18));
+				AddSequential(new Turn(-59.9f));
+				AddSequential(new DistanceDriveCommand2(16));
 
-//	AddSequential(new DistanceDriveCommand2(1.0));
+		}
+		else if(option==1)
+		{
+		    AddSequential(new SolenoidClaspCommand(0, 0));
+			AddSequential(new WaitTime(0.5));
+			AddSequential(new SolenoidClaspCommand(0, 1));
+			AddSequential(new WaitTime(0.5));
+			AddSequential(new DistanceDriveCommand2(100.6+8));
+			AddSequential(new WaitTime(0.5));
+			AddSequential(new SolenoidClaspCommand(1, 0));
+			AddSequential(new DistanceDriveCommand2(-20));
 
-	//AddSequential(new DistanceDriveCommand2(-3.0));
-
-
-	//AddSequential(new DistanceDriveCommand2(1.0));
-
-
-
-	//AddSequential(new SolenoidClaspCommand(0, 0));//set clasp solenoid to true
-
-	//Wait(10*1000);//wait 1s
-
-	//AddSequential(new SolenoidClaspCommand(0, 1));//set arm solenoid to true
-
-	//AddSequential(new DistanceDriveCommand2(1.0));
-
-	//AddSequential(new SolenoidClaspCommand(1, 0));//set clasp solenoid to false
-
-	//AddSequential(new SolenoidClaspCommand(1, 1));//set arm solenoid to false
+		}
+		else if(option==2)
+		{
+		    AddSequential(new SolenoidClaspCommand(0, 0));
+			AddSequential(new WaitTime(0.5));
+			AddSequential(new SolenoidClaspCommand(0, 1));
+			AddSequential(new WaitTime(0.5));
+			AddSequential(new DistanceDriveCommand2(100.6));
+			AddSequential(new Turn(-59.9f));
+			AddSequential(new DistanceDriveCommand2(15.18));
+			AddSequential(new WaitTime(0.5));
+			AddSequential(new SolenoidClaspCommand(1, 0));
+			AddSequential(new WaitTime(0.5));
+			AddSequential(new DistanceDriveCommand2(-15.18));
+			AddSequential(new Turn(59.9f));
+			AddSequential(new DistanceDriveCommand2(16));
+		}
 
 
 
 }
+
+
