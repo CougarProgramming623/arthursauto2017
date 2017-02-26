e7f1698aa9e23b486fbf7ec77720ab4aa63bd56f
arthurliao <liaoarthur@yahoo.com>
2017-02-26 12:18:29 -0500

    .

--- test2/src/Commands/AutoCommandGroup.cpp
+++ test2/src/Commands/AutoCommandGroup.cpp
@@ -14,24 +14,30 @@ AutoCommandGroup::AutoCommandGroup(int option1) : CommandGroup() {
 	if(option==-1)
 		return;
 
-	AddSequential(new SolenoidClaspCommand(0, 0));
+	AddSequential(new DistanceDriveCommand2(24.0)); //24 inches
+//		AddSequential(new Turn(-59.9f));
+		AddSequential(new DistanceDriveCommand2(12.0));
+
+	/*AddSequential(new SolenoidClaspCommand(0, 0));
 	AddSequential(new WaitTime(2));
 	AddSequential(new SolenoidClaspCommand(0, 1));
 	AddSequential(new WaitTime(3));
-	AddSequential(new DistanceDriveCommand(2.0));
+	AddSequential(new DistanceDriveCommand2(2.0));
 	AddSequential(new Turn(-59.9f));
-	AddSequential(new DistanceDriveCommand(1.0));
+	AddSequential(new DistanceDriveCommand2(1.0));
+
+	*/
     //option will be 0, 1, 2
 
 //	AddSequential(new DistanceDriveCommand2(1.0));
 
 
-//	AddSequential(new DistanceDriveCommand(1.0));
+//	AddSequential(new DistanceDriveCommand2(1.0));
 
-	//AddSequential(new DistanceDriveCommand(-3.0));
+	//AddSequential(new DistanceDriveCommand2(-3.0));
 
 
-	//AddSequential(new DistanceDriveCommand(1.0));
+	//AddSequential(new DistanceDriveCommand2(1.0));
 
 
 
@@ -41,7 +47,7 @@ AutoCommandGroup::AutoCommandGroup(int option1) : CommandGroup() {
 
 	//AddSequential(new SolenoidClaspCommand(0, 1));//set arm solenoid to true
 
-	//AddSequential(new DistanceDriveCommand(1.0));
+	//AddSequential(new DistanceDriveCommand2(1.0));
 
 	//AddSequential(new SolenoidClaspCommand(1, 0));//set clasp solenoid to false
 
