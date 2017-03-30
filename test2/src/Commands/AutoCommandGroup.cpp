f8ff6292c92af764757031a60413ac0c1e20214c
admin <ronniecm@gmail.com>
2017-03-29 20:00:18 -0400

    auto distance updates from owings mills

--- test2/src/Commands/AutoCommandGroup.cpp
+++ test2/src/Commands/AutoCommandGroup.cpp
@@ -8,8 +8,8 @@
 const static int BIG_TIME_OUT = 10;
 //this is for stopping the robot in case we never run enough distance to drop the Gear
 const static int SMALL_TIME_OUT = 3;
-const static double DRIVE_TO_TURN = 93.0; // measured at 94.0
-const static double DRIVE_TO_PEG = 72.25+2+2+2+2; // measured at 72.25
+const static double DRIVE_TO_TURN = 93.0+3.0; // measured at 94.0
+const static double DRIVE_TO_PEG = 72.25+2+2+2+2+3-15; // measured at 72.25
 const static double DRIVE_TO_MIDDLE = 111.0; // measured at 111.0
 const static double FRONT_HALF_LENGTH = 21.25;
 const static double BACK_HALF_LENGTH = 17.25;
@@ -54,7 +54,7 @@ AutoCommandGroup::AutoCommandGroup(int option1) :
 		AddSequential(new WaitTime(.5));
 		AddSequential(
 				new DistanceDriveCommand2(DRIVE_TO_TURN - BACK_HALF_LENGTH,
-						0.5, BIG_TIME_OUT));
+						0.5, BIG_TIME_OUT)); //Haven't yet figured out why 0 and 2 are travelling different distances
 		AddSequential(new WaitTime(.5));
 		AddSequential(new SolenoidClaspCommand(1, SPEED_SOLENOID));//slow down for turn
 		AddSequential(new Turn(TURN_DEGREES));
@@ -66,27 +66,25 @@ AutoCommandGroup::AutoCommandGroup(int option1) :
 				new DistanceDriveCommand2(DRIVE_TO_PEG - FRONT_HALF_LENGTH + 4,
 						0.6, BIG_TIME_OUT));
 		AddSequential(new WaitTime(.5));
-		AddSequential(new SolenoidClaspCommand(1, OPENCLOSE_SOLENOID)); //let go of gear
+		//AddSequential(new SolenoidClaspCommand(1, OPENCLOSE_SOLENOID)); //let go of gear
 		AddSequential(new WaitTime(.5));
-		AddSequential(new DistanceDriveCommand2(-1 * (DRIVE_TO_PEG - FRONT_HALF_LENGTH), 0.5,BIG_TIME_OUT)); // don't back up too much.  Stay in front of baseline
+		//AddSequential(new DistanceDriveCommand2(-1 * (DRIVE_TO_PEG - FRONT_HALF_LENGTH), 0.5,BIG_TIME_OUT)); // don't back up too much.  Stay in front of baseline
 
 	} else if (option == 1) {
 		AddSequential(new SolenoidClaspCommand(0, SPEED_SOLENOID));
-		// No longer need to open and close to grab gear.  It will start grabbed
-		//AddSequential(new SolenoidClaspCommand(1, OPENCLOSE_SOLENOID)); //open gear
-		//AddSequential(new WaitTime(.5));
-		//AddSequential(new DistanceDriveCommand2(4, 0.4, BIG_TIME_OUT));
 		AddSequential(new SolenoidClaspCommand(0, OPENCLOSE_SOLENOID)); //close
 		AddSequential(new WaitTime(.5)); //wait
 
 		AddSequential(new SolenoidClaspCommand(0, UPDOWN_SOLENOID)); //up
 		AddSequential(new WaitTime(.5)); //wait
-		AddSequential(new DistanceDriveCommand2(DRIVE_TO_MIDDLE - FRONT_HALF_LENGTH - BACK_HALF_LENGTH, 0.5, BIG_TIME_OUT)); //go to peg
+		AddSequential(new DistanceDriveCommand2(DRIVE_TO_MIDDLE - FRONT_HALF_LENGTH - BACK_HALF_LENGTH + 7.5, 0.5, BIG_TIME_OUT)); //go to peg
 		AddSequential(new WaitTime(.5)); //wait
+		AddSequential(new SolenoidClaspCommand(0, GEAR_PUSHER_SOLENOID)); //push gear
+		AddSequential(new WaitTime(0.5));
 		AddSequential(new SolenoidClaspCommand(1, OPENCLOSE_SOLENOID)); //open
 		AddSequential(new WaitTime(1.0));
 		AddSequential(new DistanceDriveCommand2(-40, 0.5, BIG_TIME_OUT)); //drive back
-
+		AddSequential(new SolenoidClaspCommand(1, GEAR_PUSHER_SOLENOID)); //unpush gear
 	} else if (option == 2) {
 		AddSequential(new SolenoidClaspCommand(0, SPEED_SOLENOID)); //high gear
 		// No longer need to open and close to grab gear.  It will start grabbed
@@ -110,12 +108,9 @@ AutoCommandGroup::AutoCommandGroup(int option1) :
 				new DistanceDriveCommand2(DRIVE_TO_PEG - FRONT_HALF_LENGTH + 4,
 						0.6, BIG_TIME_OUT)); //drive to peg
 		AddSequential(new WaitTime(.5)); //wait
-		AddSequential(new SolenoidClaspCommand(1, OPENCLOSE_SOLENOID)); //open
+		//AddSequential(new SolenoidClaspCommand(1, OPENCLOSE_SOLENOID)); //open
 		AddSequential(new WaitTime(.5)); //wait
-		AddSequential(
-				new DistanceDriveCommand2(
-						-1 * (DRIVE_TO_PEG - FRONT_HALF_LENGTH), 0.5,
-						BIG_TIME_OUT)); //drive back
+		//AddSequential(new DistanceDriveCommand2(-1 * (DRIVE_TO_PEG - FRONT_HALF_LENGTH), 0.5, BIG_TIME_OUT)); //drive back
 		/*
 		 AddSequential(new Turn(59.9f));
 		 AddSequential(new DistanceDriveCommand2(110, 0.5, BIG_TIME_OUT));
